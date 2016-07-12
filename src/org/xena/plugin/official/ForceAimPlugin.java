4696cb0a0bc669711d167e327cb4b72d6f23af21
Jonathan Beaudoin <jonathanbeaudoin1996@gmail.com>
2016-07-11 22:44:11 -0400

    Added unique color for bomb carrier, added bomb + chicken esp

--- src/org/xena/plugin/official/ForceAimPlugin.java
+++ src/org/xena/plugin/official/ForceAimPlugin.java
@@ -12,69 +12,47 @@ import org.xena.plugin.PluginManifest;
 import org.xena.plugin.utils.AngleUtils;
 
 import java.util.Collection;
-import java.util.Random;
 
 @PluginManifest(name = "Aim Assist", description = "Helps you to stay on target.")
 public final class ForceAimPlugin extends Plugin {
-
-    private static final int[] targetBones = {6};
 
     private final AngleUtils aimHelper;
 
     public ForceAimPlugin(Logger logger, Xena xena) {
         super(logger, xena);
-        aimHelper = new AngleUtils(this, targetBones, 40.5F, 1.7F, 2.5F, 1.7F, 2.5F);
+        aimHelper = new AngleUtils(this, 40.5F, 1.7F, 2.5F, 1.7F, 2.5F);
     }
 
     private final float[] aim = new float[3];
-    private final float[] enemyPosition = new float[3];
-    private final float[] angle = new float[3];
 
-    private int targetBone = -1;
-
-    private final Random random = new Random();
-
-    private int lastCrosshair = 0;
+    private Player lastTarget = null;
 
     @Override
-    public void pulse(ClientState clientState, Me me, GameEntity[] entities, Collection<Player> players) {
+    public void pulse(ClientState clientState, Me me, Collection<GameEntity> players) {
         if (NativeKeyUtils.isKeyDown(164)) {
-            int target = aimHelper.getCrosshairID(me);
-            if (lastCrosshair > 0 && target == -1) {
-                if (!aimHelper.isDead(lastCrosshair) && aimHelper.isSpotted(lastCrosshair)) {
-                    target = lastCrosshair;
+            Player target = me.getTarget();
+            if (lastTarget != null && target == null) {
+                if (!lastTarget.isDead() && lastTarget.isSpotted()) {
+                    target = lastTarget;
                 } else {
-                    lastCrosshair = -1;
+                    lastTarget = null;
                 }
             }
 
-            if (target < 0) {
+            if (target == null) {
                 return;
             }
 
-            if (target >= 0) {
-                try {
-                    if (targetBone == -1) {
-                        targetBone = targetBones[random.nextInt(targetBones.length)];
-                    }
-                    if (aimHelper.canShoot(me, target) && aimHelper.getBones(target, enemyPosition, targetBone) != null) {
-                        aimHelper.velocityComp(me, target, enemyPosition);
-                        aimHelper.calculateAngle(me, me.getPosition(), enemyPosition, aim);
-                        aimHelper.normalizeAngle(aim);
-                        aimHelper.getAngle(angle);
-                        aimHelper.normalizeAngle(angle);
-                        aimHelper.setAngleSmooth(aim, angle);
-                    } else {
-                        lastCrosshair = -1;
-                        return;
-                    }
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
+            if (aimHelper.canShoot(me, target)) {
+                aimHelper.velocityComp(me, target, target.getBones());
+                aimHelper.calculateAngle(me, me.getPosition(), target.getBones(), aim);
+                aimHelper.setAngleSmooth(aim, target.getViewAngles());
+                lastTarget = target;
+            } else {
+                lastTarget = null;
             }
-            lastCrosshair = target;
         } else {
-            lastCrosshair = -1;
+            lastTarget = null;
         }
     }
 
--- src/org/xena/plugin/official/GlowESPPlugin.java
+++ src/org/xena/plugin/official/GlowESPPlugin.java
@@ -1,10 +1,7 @@
 package org.xena.plugin.official;
 
 import org.xena.Xena;
-import org.xena.cs.ClientState;
-import org.xena.cs.GameEntity;
-import org.xena.cs.Me;
-import org.xena.cs.Player;
+import org.xena.cs.*;
 import org.xena.logging.Logger;
 import org.xena.plugin.Plugin;
 import org.xena.plugin.PluginManifest;
@@ -13,7 +10,7 @@ import java.util.Collection;
 
 import static org.abendigo.offsets.Offsets.m_dwGlowObject;
 
-@PluginManifest(name = "Glow ESP", description = "Make players glow on your screen.")
+@PluginManifest(name = "Glow ESP", description = "Make entities glow on your screen.")
 public final class GlowESPPlugin extends Plugin {
 
     public GlowESPPlugin(Logger logger, Xena xena) {
@@ -21,30 +18,47 @@ public final class GlowESPPlugin extends Plugin {
     }
 
     @Override
-    public void pulse(ClientState clientState, Me me, GameEntity[] entities, Collection<Player> players) {
+    public void pulse(ClientState clientState, Me me, Collection<GameEntity> players) {
         int pointerGlow = client().readInt(m_dwGlowObject);
         int glowObjectCount = client().readInt(m_dwGlowObject + 4);
 
         for (int i = 0; i < glowObjectCount; i++) {
-            int glowObjectPointer = pointerGlow + (i * 56);
-            int glowObjectEntityAddress = process().readInt(glowObjectPointer);
-            if (glowObjectEntityAddress == 0) {
-                continue;
-            }
-            for (Player p : players) {
-                if (me.equals(p)) {
+            try {
+                int glowObjectPointer = pointerGlow + (i * 56);
+                int glowObjectEntityAddress = process().readInt(glowObjectPointer);
+                if (glowObjectEntityAddress < 0x200) {
                     continue;
                 }
-                if (glowObjectEntityAddress == p.address()) {
+
+                GameEntity entity = Game.current().entities().get(glowObjectEntityAddress);
+                if (entity != null) {
                     float red = 0.8784314f, green = 0.6862745f, blue = 0.3372549f;
                     float opacity = 0.6F;
-                    if (p.getTeam() == 3) {
-                        red = 0.44705883f;
-                        green = 0.60784316f;
-                        blue = 0.8666667f;
-                    }
-                    if (p.getTeam() != me.getTeam() && me.getTarget() == p.address()) {
-                        green = 215;
+                    if (entity.getClassId() == 35) {
+                        Player player = entity.asPlayer();
+                        if (player.getTeam() == 3) {
+                            red = 0.44705883f;
+                            green = 0.60784316f;
+                            blue = 0.8666667f;
+                        }
+                        if (player.getTeam() != me.getTeam() && me.getTarget() == player) {
+                            green = .8f;
+                        }
+                        if (player.isHasBomb()) {
+                            red = 1.0f;
+                            green = 0.0f;
+                            blue = 0.0f;
+                        }
+                    } else if (entity.getClassId() == 105 || entity.getClassId() == 29) {
+                        red = 1.0f;
+                        green = 0.0f;
+                        blue = 0.0f;
+                    } else if (entity.getClassId() == 31) {
+                        red = 0.0f;
+                        green = 1.0f;
+                        blue = 0.0f;
+                    } else {
+                        continue;
                     }
                     process().writeFloat(glowObjectPointer + 0x4, red);
                     process().writeFloat(glowObjectPointer + 0x8, green);
@@ -53,6 +67,7 @@ public final class GlowESPPlugin extends Plugin {
                     process().writeBoolean(glowObjectPointer + 0x24, true);
                     process().writeBoolean(glowObjectPointer + 0x25, false);
                 }
+            } catch (Throwable ignored) {
             }
         }
         sleep(64);
