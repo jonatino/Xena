da33f747c0ee138d7fb64958801eb5c3530da8c0
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-07-16 19:11:15 -0400

    Added build/run commands for easy execution

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+title Xena Builder
+call gradlew installDist
+echo.
+pause
---
+++ run.bat
@@ -0,0 +1,15 @@
+@echo off
+title Abendigo
+
+set bat="./build/install/Xena/bin/Xena.bat"
+
+:loop
+if exist %bat% (
+    call %bat%
+    pause
+) else (
+    call build.bat
+    cls
+    title Xena
+    goto loop
+)
