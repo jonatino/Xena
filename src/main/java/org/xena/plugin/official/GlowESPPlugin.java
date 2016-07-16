94237952a6f4c2c14703a798289c197b0384174c
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-07-16 17:08:31 -0400

    Cleaned up ESP a lot

--- src/main/java/org/xena/plugin/official/GlowESPPlugin.java
+++ src/main/java/org/xena/plugin/official/GlowESPPlugin.java
@@ -1,63 +1,60 @@
 package org.xena.plugin.official;
 
+import org.xena.Indexer;
 import org.xena.Xena;
 import org.xena.cs.*;
 import org.xena.logging.Logger;
 import org.xena.plugin.Plugin;
 import org.xena.plugin.PluginManifest;
 
-import java.util.Collection;
+import static org.abendigo.offsets.Offsets.m_dwGlowObject;
 
 @PluginManifest(name = "Glow ESP", description = "Make entities glow on your screen.")
 public final class GlowESPPlugin extends Plugin {
+
+    private static final float[] TEAM_CT = {114.0f, 155.0f, 221.0f, 153.0f};
+    private static final float[] TEAM_T = {224.0f, 175.0f, 86.0f, 153.0f};
+    private static final float[] BOMB_CARRY = {255f, 0.0f, 0.0f, 200.0f};
 
     public GlowESPPlugin(Logger logger, Xena xena) {
         super(logger, xena);
     }
 
     @Override
-    public void pulse(ClientState clientState, Me me, Collection<GameEntity> entities) {
-        for (GameEntity entity : entities) {
-            try {
-                float red = 0.8784314f, green = 0.6862745f, blue = 0.3372549f;
-                float opacity = 0.6F;
-                if (entity.type() == EntityType.CCSPlayer && entity.isPlayer()) {
+    public void pulse(ClientState clientState, Me me, Indexer<GameEntity> entities) {
+        long pointerGlow = client().readUnsignedInt(m_dwGlowObject);
+        long glowObjectCount = client().readUnsignedInt(m_dwGlowObject + 4);
+
+        for (int i = 0; i < glowObjectCount; i++) {
+            long glowObjectPointer = pointerGlow + (i * 56);
+            long glowEntityAddress = process().readUnsignedInt(glowObjectPointer);
+            GameEntity entity = Game.current().get(glowEntityAddress);
+
+            if (entity != null) {
+                try {
                     Player player = entity.asPlayer();
-                    if (player.getTeam() == 3) {
-                        red = 0.44705883f;
-                        green = 0.60784316f;
-                        blue = 0.8666667f;
+                    float[] c = getColor(player);
+                    for (int x = 0; x < 4; x++) {
+                        process().writeFloat(glowObjectPointer + (x + 1) * 4, c[x] / 255.0f);
                     }
-                    if (player.getTeam() != me.getTeam() && me.getTarget() == player) {
-                        green = .8f;
-                    }
-                    if (player.isHasBomb()) {
-                        red = 1.0f;
-                        green = 0.0f;
-                        blue = 0.0f;
-                    }
-                } else if (entity.type() == EntityType.CPlantedC4 || entity.type() == EntityType.CC4) {
-                    red = 1.0f;
-                    green = 0.0f;
-                    blue = 0.0f;
-                } else if (entity.type() == EntityType.CChicken) {
-                    red = 0.0f;
-                    green = 1.0f;
-                    blue = 0.0f;
-                } else {
-                    continue;
+                    process().writeBoolean(glowObjectPointer + 0x24, true);
+                    process().writeBoolean(glowObjectPointer + 0x25, false);
+                } catch (Throwable ignored) {
+                    ignored.printStackTrace();
                 }
-                process().writeFloat(entity.getGlowObjectPointer() + 0x4, red);
-                process().writeFloat(entity.getGlowObjectPointer() + 0x8, green);
-                process().writeFloat(entity.getGlowObjectPointer() + 0xC, blue);
-                process().writeFloat(entity.getGlowObjectPointer() + 0x10, opacity);
-                process().writeBoolean(entity.getGlowObjectPointer() + 0x24, true);
-                process().writeBoolean(entity.getGlowObjectPointer() + 0x25, false);
-            } catch (Throwable ignored) {
-                ignored.printStackTrace();
             }
         }
         sleep(128);
+    }
+
+    private float[] getColor(Player player) {
+        if (player.getTeam() == 3) {
+            return TEAM_CT;
+        }
+        if (player.isHasBomb()) {
+            return BOMB_CARRY;
+        }
+        return TEAM_T;
     }
 
 }
