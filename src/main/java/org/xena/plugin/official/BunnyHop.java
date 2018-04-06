1d7c90d2228faf07ba45d96c4cbfac538161d3b7
Jerem584 <31523935+Jerem584@users.noreply.github.com>
2018-04-06 09:37:42 +0200

    Updated bhop

--- src/main/java/org/xena/plugin/official/BunnyHop.java
+++ src/main/java/org/xena/plugin/official/BunnyHop.java
@@ -12,30 +12,26 @@ import java.awt.event.KeyEvent;
 
 import static org.xena.offsets.OffsetManager.clientModule;
 import static org.xena.offsets.OffsetManager.process;
+
 import static org.xena.offsets.offsets.NetVarOffsets.fFlags;
 
 @PluginManifest(name = "No Hand", description = "No hand.")
 public class BunnyHop extends Plugin {
-
-    public static int FL_ONGROUND = (1 << 0); // At rest / on the ground
-
+        public static int FL_ONGROUND = 257; // At rest / on the ground
     @Override
     public void pulse(ClientState clientState, Me me, Indexer<GameEntity> entities)
     {
         long m_fFlags = process().readLong(me.address() + fFlags);
-        int FL_ONGROUND = 257; // At rest / on the ground
-        int jump = 0x4F1AAF4;
-
         if(NativeKeyUtils.isKeyDown(KeyEvent.VK_SPACE))
         {
             if (false && (m_fFlags & FL_ONGROUND) > 0)
                 process().writeInt(jump, 6);
             else if ((m_fFlags & FL_ONGROUND) > 0) {
                 try {
-                    Thread.sleep(5 + (int) (Math.random() * 15));
-                    clientModule().writeInt(jump, 5);
+                    Thread.sleep(5 + (int) (Math.random() * 15)); /* Play with thread sleep for perfect bhop */
+                    clientModule().writeInt(dwForceJump, 5); /* you may need to importe this offsets */
                     Thread.sleep(5 + (int) (Math.random() * 15 ));
-                    clientModule().writeInt(jump, 4);
+                    clientModule().writeInt(dwForceJump, 4);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
